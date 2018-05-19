25173e56b574e44bba7661e9a3ec93ff21739f3f
Duknam Yoo <yoo2001818@gmail.com>
2018-05-19 23:59:55 +0900

    Implement parser using stack

--- README.md
+++ README.md
@@ -3,14 +3,17 @@ GLSL ES 3 to 1 transpiler
 
 # TODO
 - [ ] Convert `#version` to 100
+- [ ] Create uniform for `textureSize`, `texelFetch`
 - [ ] Polyfill `textureSize(sampler, lod)`
 - [ ] Polyfill `texelFetch(sampler, ivec2Position, lod)`
-- [ ] Detect type of sampler
+- [ ] Detect type of sampler (For converting `texture`)
 - [ ] Convert `texture` to `texture2D`, `textureCube`
 - [ ] Polyfill `invert` and `transpose`
 - [ ] Polyfill bitwise operations
 - [ ] Convert `in` and `out`
 - [ ] Convert `out` to `gl_FragColor`
+- [ ] Convert `out` to `gl_FragData` (GL_EXT_draw_buffers)
+- [ ] Strip `layout` in uniform
 - [ ] Convert uniform layout to struct
 - [ ] Apply `OES_standard_derivatives`
 - [ ] Apply `EXT_shader_texture_lod`
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
+import { Lexer, Token } from 'moo';
 import tokenize from './tokenizer';
 
+type Scope = { [key: string]: any };
+interface State {
+  type: 'fragment' | 'vertex';
+  scopes: Scope[];
+  fragColor?: string;
+}
+
 export default function convert(code: string, type: 'fragment' | 'vertex') {
-  let result = [];
+  let result: string[] = [];
   let tokenizer = tokenize(code);
-  let token;
-  let scope: { [key: string]: any } = {};
-  let state = '';
+  main(tokenizer, { type, scopes: [{}] }, result);
+  return result;
+}
+
+function main(tokenizer: Lexer, state: State, result: string[]) {
+  let nextToken = tokenizer.next();
+  while (nextToken != null) {
+    match(nextToken, {
+      in: () => result.push(state.type === 'vertex' ? 'attribute': 'varying'),
+      out: () => state.type === 'vertex'
+        ? result.push('varying')
+        : grabFragColor(tokenizer, state),
+      identifier: (token: Token) => {
+        if (token.value === 'gl_FragColor' && state.fragColor) {
+          result.push(state.fragColor);
+        } else {
+          result.push(token.value);
+        }
+      }
+    })
+    nextToken = tokenizer.next();
+  }
+  /*
   while (token = tokenizer.next()) {
     switch (token.type) {
       case 'in':
@@ -36,4 +64,16 @@ export default function convert(code: string, type: 'fragment' | 'vertex') {
     }
     console.log(token.type, token.value);
   }
+  */
+}
+
+function getFragColor(tokenizer: Lexer, state: State, result: string[]) {
+}
+
+function match(
+  token: Token, patterns: { [key: string]: (token: Token) => any },
+) {
+  if (patterns[token.type] != null) return patterns[token.type](token);
+  if (patterns['otherwise'] != null) return patterns['otherwise'](token);
+  throw new Error('Unexpected token ' + token.value);
 }
