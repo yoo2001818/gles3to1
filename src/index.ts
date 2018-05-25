8f7397383acdc3fd583f49b3e923110dc6e28ad1
Duknam Yoo <yoo2001818@gmail.com>
2018-05-25 23:55:41 +0900

    Reimplement translator using new structure

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ class State {
       return this.buffer.shift();
     }
     let token = this.lexer.next();
-    if (token.type === 'WS') {
+    if (token == null) return token;
+    if (token.type === 'WS' || token.type === 'comment') {
       if (outputWs) this.output.push(token.value);
       return this.next(outputWs);
     }
@@ -34,6 +35,9 @@ function match<T>(
   state: State, patterns: { [key: string]: (token: Token) => T | any },
 ): T | any {
   let token = state.next();
+  if (token == null && patterns['eof'] != null) {
+    return patterns['eof'](token);
+  }
   if (patterns[token.type] != null) return patterns[token.type](token);
   if (patterns['otherwise'] != null) return patterns['otherwise'](token);
   throw new Error('Unexpected token ' + token.value);
@@ -41,6 +45,7 @@ function match<T>(
 
 function pull(state: State, type: string) {
   let token = state.next();
+  if (token == null) throw new Error('Unexpected end of input');
   if (token.type !== type) {
     throw new Error('Token error; expected ' + type + ' but received ' +
       token.type);
@@ -49,13 +54,15 @@ function pull(state: State, type: string) {
 }
 
 function pullIf<T>(
-  state: State, type: string, then: (token: Token) => T,
-): T | false {
+  state: State, type: string, then?: (token: Token) => T,
+): T | false | true {
   let token = state.next();
+  if (token == null) throw new Error('Unexpected end of input');
   if (token.type !== type) {
     state.push(token);
     return false;
   }
+  if (then == null) return true;
   return then(token);
 }
 
@@ -81,10 +88,12 @@ function main(state: State) {
     identifier: (token: Token) => {
       let fragColorIndex = state.fragColors.indexOf(token.value);
       if (fragColorIndex !== -1) {
-        state.output.push(state.fragColors[fragColorIndex]);
+        state.output.push(fragColorIndex === 0
+          ? 'gl_FragColor'
+          : `gl_FragData[${fragColorIndex}]`);
       } else if (token.value === 'texture') {
-        state.push(token);
-        convertTexture(state);
+        state.output.push(token.value);
+        // convertTexture(state);
       } else {
         state.output.push(token.value);
       }
@@ -104,8 +113,8 @@ function main(state: State) {
       switch (token.value) {
         case 'sampler2D':
         case 'samplerCube':
-          state.output.push(token.value);
-          getVarDecl(token);
+          state.push(token);
+          getVarDecl(state);
           break;
         default:
           state.output.push(token.value);
@@ -123,110 +132,31 @@ function main(state: State) {
       }
     },
     otherwise: (token: Token) => state.output.push(token.value),
-  }) !== false);
-  console.log(result.join(''));
+    eof: () => true,
+  }) !== true);
+  console.log(state.output.join(''));
 }
 
-function getFragColor(tokenizer: Lexer, state: State, result: string[]) {
-  let token = tokenizer.next();
-  while (token != null) {
-    let doContinue = true;
-    match(token, {
-      precisionIdentifier: () => {},
-      type: () => {},
-      WS: () => {},
-      comment: () => {},
-      identifier: (token: Token) => {
-        state.fragColor = token.value;
-      },
-      semicolon: () => doContinue = false,
-    });
-    if (!doContinue) break;
-    token = tokenizer.next();
+function getFragColor(state: State) {
+  pullIf(state, 'precisionIdentifier');
+  pull(state, 'type');
+  let name = pull(state, 'identifier');
+  state.fragColors.push(name.value);
+  while (true) {
+    let token = state.next();
+    if (token == null || token.type === 'semicolon') break;
   }
 }
 
-function getVarDecl(
-  typeToken: Token, tokenizer: Lexer, state: State, result: string[],
-) {
-  let typeName = typeToken.value;
-  let name: string | null = null;
-  let token = tokenizer.next();
-  while (token != null) {
-    let doContinue = true;
-    result.push(token.value);
-    match(token, {
-      precisionIdentifier: () => {},
-      type: () => {},
-      WS: () => {},
-      comment: () => {},
-      identifier: (token: Token) => {
-        name = token.value;
-      },
-      semicolon: () => doContinue = false,
-    });
-    if (!doContinue) {
-      if (name == null) {
-        throw new Error('VariatypeTokeble declaration name must be specified')
-      }
-      let scope = statfunction match<T>(
-  token: Token, patterns: { [key: string]: (token: Token) => T | any },
-): T | any {
-  if (patterns[token.type] != null) return patterns[token.type](token);
-  if (patterns['otherwise'] != null) return patterns['otherwise'](token);
-  throw new Error('Unexpected token ' + token.value);
-}
-.scopes[state.scopes.length - 1];
-      scope[name] = tyfunction match<T>(
-  token: Token, patterns: { [key: string]: (token: Token) => T | any },
-): T | any {
-  if (patterns[token.type] != null) return patterns[token.type](token);
-  if (patterns['otherwise'] != null) return patterns['otherwise'](token);
-  throw new Error('Unexpected token ' + token.value);
-}
-eName;
-      console.log(scopfunction match<T>(
-  token: Token, patterns: { [key: string]: (token: Token) => T | any },
-): T | any {
-  if (patterns[token.type] != null) return patterns[token.type](token);
-  if (patterns['otherwise'] != null) return patterns['otherwise'](token);
-  throw new Error('Unexpected token ' + token.value);
-}
-);
-      break;
-    }
-    token = tokenizer.next();
+function getVarDecl(state: State) {
+  let type = pull(state, 'type');
+  state.output.push(type.value);
+  let name = pull(state, 'identifier');
+  state.output.push(name.value);
+  while (true) {
+    let token = state.next();
+    state.output.push(token.value);
+    if (token == null || token.type === 'semicolon') break;
   }
-}
-
-function convertTexture(
-  funcToken: Token, tokenizer: Lexer, state: State, result: string[],
-) {
-  let buffer: string[] = [];
-  // texture(abc, def)
-  // Wouldn't it be better to use actual parser?
-  let token = tokenizer.next();
-  let depth = 1;
-  let args: string[] = [];
-  while (token != null) {
-    let doContinue = true;
-    buffer.push(token.value);
-    match(token, {
-      WS: () => {},
-      leftParen: () => {
-        depth += 1;
-      },
-      rightParen: () => {
-        depth -= 1;
-        if (depth <= 0) doContinue = false;
-      },
-      comma: () => {
-      },
-      identifier: (token: Token) => {
-        args.push(token.value);
-      },
-    });
-    if (!doContinue) break;
-    token = tokenizer.next();
-  }
+  state.scopes[state.scopes.length - 1][name.value] = type.value;
 }
