c2e2d152648e63d4f48d83c47c32e38f52034e9e
Duknam Yoo <yoo2001818@gmail.com>
2018-05-30 23:55:57 +0900

    Add function / init declaration

--- src/token.ts
+++ src/token.ts
@@ -7,42 +7,122 @@ type UintConstant = Token & { type: 'uintConstant', value: number };
 type FloatConstant = Token & { type: 'floatConstant', value: number };
 type BoolConstant = Token & { type: 'boolConstant', value: boolean };
 
-type PrimaryExpression = Identifier | IntConstant | UintConstant |
-  FloatConstant | BoolConstant;
 type ArrayExpression = {
-  type: 'arrayExpression', array: PostfixExpression, index: PrimaryExpression,
+  type: 'arrayExpression', array: Expression, index: Expression,
 };
+
 type MemberExpression = {
-  type: 'memberExpression', object: PostfixExpression, index: Identifier, 
+  type: 'memberExpression', object: Expression, index: Identifier, 
 };
+
 type CallExpression = {
   type: 'callExpression',
-  callee: PostfixExpression | TypeConstant | null,
+  callee: Expression | TypeConstant | null,
   arguments: Expression[],
 };
+
 type UpdateExpression = {
   type: 'updateExpression',
   operator: '--' | '++',
   prefix: boolean,
-  argument: PostfixExpression, 
+  argument: Expression, 
 };
 
-type PostfixExpression = PrimaryExpression | 
-  ArrayExpression | MemberExpression | CallExpression | UpdateExpression;
-type UnaryExpression = PostfixExpression | {
+type UnaryExpression = {
   type: 'unaryExpression',
   operator: '+' | '-' | '!' | '~',
-  argument: PostfixExpression,
+  argument: Expression,
 };
 
-type BinaryExpression = UnaryExpression | {
+type BinaryExpression = {
   type: 'binaryExpression',
   operator: '*' | '/' | '%' | '+' | '-' | '<<' | '>>' |
     '<' | '>' | '<=' | '>=' | '==' | '!=' | '&' | '^' | '|' |
     '&&' | '^^' | '||';
-  left: PostfixExpression,
-  right: PostfixExpression,
+  left: Expression,
+  right: Expression,
+};
+
+type ConditionalExpression = {
+  type: 'conditionalExpression',
+  test: Expression,
+  consequent: Expression,
+  alternate: Expression,
+};
+
+type AssignmentExpression = {
+  type: 'assignmentExpression',
+  operator: '=' | '*=' | '/=' | '%=' | '+=' | '-=' | '<<=' | '>>=' |
+    '&=' | '^=' | '|=',
+  left: UnaryExpression,
+  right: Expression,
+};
+
+type SeqeunceExpression = {
+  type: 'sequenceExpression',
+  expressions: Expression[],
+};
+
+type PrimaryExpression = Identifier | IntConstant | UintConstant |
+  FloatConstant | BoolConstant;
+type PostfixExpression = PrimaryExpression | 
+  ArrayExpression | MemberExpression | CallExpression | UpdateExpression;
+type ConstantExpression = PrimaryExpression | PostfixExpression |
+  UnaryExpression | BinaryExpression | ConditionalExpression;
+type Expression = ConstantExpression | AssignmentExpression |
+  SeqeunceExpression
+
+type LayoutQualifierId = { name: string, value: number | null };
+type TypeQualifier = {
+  layout: LayoutQualifierId[],
+  storage: string | null,
+  interpolation: string | null,
+  invariant: string | null,
+};
+type TypeSpecifier = {
+  precision: string | null,
+  valueType: TypeConstant,
+  isArray: boolean,
+  size: ConstantExpression | null,
+};
+type FullType = TypeQualifier & TypeSpecifier;
+type ParameterDeclaration = TypeSpecifier & {
+  name: string,
+  isConst: boolean,
+  qualifier: null | 'in' | 'out' | 'inout',
+};
+
+type FunctionPrototype = {
+  type: 'functionPrototype',
+  name: string,
+  returns: FullType,
+  arguments: ParameterDeclaration[],
+};
+
+type InitDeclaration = {
+  type: 'initDeclaration',
+  valueType: FullType,
+  name: string,
+  value: null | Expression,
+};
+
+type InitDeclarationList = {
+  type: 'initDeclarationList',
+  declarations: InitDeclaration[],
+};
+
+type PrecisionDeclaration = {
+  type: 'precisionDeclaration',
+  precision: string,
+  valueType: TypeConstant,
 };
 
 
-type Expression = PostfixExpression;
+/*
+type_qualifier IDENTIFIER LEFT_BRACE struct_declaration_list RIGHT_BRACE SEMICOLON
+type_qualifier IDENTIFIER LEFT_BRACE struct_declaration_list RIGHT_BRACE
+IDENTIFIER SEMICOLON
+type_qualifier IDENTIFIER LEFT_BRACE struct_declaration_list RIGHT_BRACE
+IDENTIFIER LEFT_BRACKET constant_expression RIGHT_BRACKET SEMICOLON
+type_qualifier SEMICOLON
+*/
